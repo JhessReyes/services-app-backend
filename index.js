77fa0d488fb9e854ca555d71c4cd8506c2a4f6c3
JhessReyes <jhessaureyes@gmail.com>
2022-10-27 16:53:31 -0600

    deleted all

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require( 'express');
-const app = require('./src/app.js');
-/* const app = express(); */
-app.listen(app.get('port'));
-
-console.log("Server on Port", app.get('port'));
--- src/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-const Router = require("express");
-const users = require("../controllers/user.controller");
-
-const router = Router();
-
-router.get("/getUser", users.getUsers);
-
-module.exports =  router;
