ad447174dc97a6ebae140dcd80e27592e8a0a0e3
JhessReyes <jhessaureyes@gmail.com>
2022-10-27 02:36:49 -0600

    a

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const config = require("./config.js");
 const userRoutes = require("./routes/user.routes.js");
-const bodyParser = require('body-parser')
+/* const bodyParser = require('body-parser') */
 /* const userRoutes = require("./routes/user.routes.js"); */
 
 const app = express();
 
 //setings
 app.set("port", 3000 /* config.port */);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+/* app.use(bodyParser.json());
+app.use(bodyParser.urlencoded()); */
 app.use(userRoutes);
 module.exports = app;
