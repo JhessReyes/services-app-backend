523ed98338a6475ab2b72c123520f0e9b8bc2bf7
JhessReyes <jhessaureyes@gmail.com>
2022-10-29 20:29:33 -0600

    updatePaymentPeriod

--- src/controllers/paymentPeriod.controller.js
+++ src/controllers/paymentPeriod.controller.js
@@ -34,3 +34,29 @@ export const insertPaymentPeriod = async (req, res) => {
     });
   }
 };
+
+export const updatePaymentPeriod = async (req, res) => {
+  try {
+    const { id, period, interest } = req.body;
+    if (id == null) {
+      return res.status(400).json({
+        message: "Bad Request. (id) is requiered",
+      });
+    }
+
+    const pool = await getConection();
+    const result = await pool
+      .request()
+      .input("id", id)
+      .input("period", sql.Int, period)
+      .input("interest", sql.Decimal(10, 2), interest)
+      .query("exec pc_update_payment_period @id, @period, @interest");
+    res.status(200).json(result.recordset[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server Error",
+      error: error.message,
+    });
+  }
+};
--- src/routes/paymentPeriod.routes.js
+++ src/routes/paymentPeriod.routes.js
@@ -1,7 +1,11 @@
 import Router from "express";
-import { insertPaymentPeriod } from "../controllers/paymentPeriod.controller";
+import {
+  insertPaymentPeriod,
+  updatePaymentPeriod,
+} from "../controllers/paymentPeriod.controller";
 const router = Router();
 
 router.post("/insertPaymentPeriod", insertPaymentPeriod);
+router.put("/updatePaymentPeriod", updatePaymentPeriod);
 
 export default router;
