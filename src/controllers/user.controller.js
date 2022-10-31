8af1fa620c132d3644b6b7b8f902e4ab014e0ee1
JhessReyes <jhessaureyes@gmail.com>
2022-10-30 19:07:47 -0600

    user validate user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -53,79 +53,88 @@ export const validateUser = async (req, res) => {
 };
 
 export const getUserById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const pool = await getConection();
-    const result = await pool
-      .request()
-      .input("id", id)
-      .query("SELECT * FROM tbUser WHERE id = @id");
-    res.status(200).json(result.recordset[0]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: "Server Error",
-      error: error.message,
-    });
-  }
+  const validated = await validateSession(req);
+  if (validated.status) {
+    try {
+      const { id } = req.params;
+      const pool = await getConection();
+      const result = await pool
+        .request()
+        .input("id", id)
+        .query("SELECT * FROM tbUser WHERE id = @id");
+      res.status(200).json(result.recordset[0]);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "Server Error",
+        error: error.message,
+      });
+    }
+  } else res.status(401).json({ message: "User Unauthorized", status: false });
 };
 
 export const updateUser = async (req, res) => {
-  try {
-    const { id, name, mail, password } = req.body;
-    if (id == null) {
-      return res.status(400).json({
-        message: "Bad Request. (id) is required",
+  const validated = await validateSession(req);
+  if (validated.status) {
+    try {
+      const { id, name, mail, password } = req.body;
+      if (id == null) {
+        return res.status(400).json({
+          message: "Bad Request. (id) is required",
+        });
+      }
+
+      const pool = await getConection();
+      const result = await pool
+        .request()
+        .input("id", id)
+        .input("name", sql.VarChar, name)
+        .input("mail", sql.VarChar, mail)
+        .input("password", sql.VarChar, password)
+        .query("exec pc_update_user @id, @name, @mail, @password");
+      res.status(200).json(result.recordset[0]);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "Server Error",
+        error: error.message,
       });
     }
-
-    const pool = await getConection();
-    const result = await pool
-      .request()
-      .input("id", id)
-      .input("name", sql.VarChar, name)
-      .input("mail", sql.VarChar, mail)
-      .input("password", sql.VarChar, password)
-      .query("exec pc_update_user @id, @name, @mail, @password");
-    res.status(200).json(result.recordset[0]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: "Server Error",
-      error: error.message,
-    });
-  }
+  } else res.status(401).json({ message: "User Unauthorized", status: false });
 };
 
 export const insertUser = async (req, res) => {
-  try {
-    const { name, mail, password } = req.body;
-    if (name == null || mail == null || password == null) {
-      return res.status(400).json({
-        message: "Bad Request. Please fill all fields",
+  const validated = await validateSession(req);
+  if (validated.status) {
+    try {
+      const { name, mail, password } = req.body;
+      if (name == null || mail == null || password == null) {
+        return res.status(400).json({
+          message: "Bad Request. Please fill all fields",
+        });
+      }
+
+      const pool = await getConection();
+      const result = await pool
+        .request()
+        .input("name", sql.VarChar, name)
+        .input("mail", sql.VarChar, mail)
+        .input("password", sql.VarChar, password)
+        .query("exec pc_insert_user @name, @mail, @password");
+      res.status(200).json(result.recordset[0]);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "Server Error",
+        error: error.message,
       });
     }
-
-    const pool = await getConection();
-    const result = await pool
-      .request()
-      .input("name", sql.VarChar, name)
-      .input("mail", sql.VarChar, mail)
-      .input("password", sql.VarChar, password)
-      .query("exec pc_insert_user @name, @mail, @password");
-    res.status(200).json(result.recordset[0]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: "Server Error",
-      error: error.message,
-    });
-  }
+  } else res.status(401).json({ message: "User Unauthorized", status: false });
 };
 
 export const getUsers = async (req, res) => {
-  const validado = await validateSession(req);
-  if (validado.status) {
+  const validated = await validateSession(req);
+  if (validated.status) {
     try {
       const pool = await getConection();
       const result = await pool.request().query("SELECT * FROM tbUser");
