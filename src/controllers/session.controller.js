996677c5c38b062cd70c81639876f9fc6148f6e6
JhessReyes <jhessaureyes@gmail.com>
2022-10-30 18:53:10 -0600

    send Status

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -1,15 +1,16 @@
 const getConection = require("../databases/conection");
 const sql = require("mssql");
 
-export const validateSession = async (req) => {
+export const validateSession = async (req, res) => {
   try {
     const { cookies } = req;
 
     if (!cookies.accessToken) {
+      res.sendStatus(401);/* 
       return {
         status: query.recordset[0].status,
       };
-    }
+ */    }
 
     const pool = await getConection();
     const query = await pool
