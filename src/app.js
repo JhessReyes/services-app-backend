4e259fd4bc82680cc468d10824a982d0e68c6436
JhessReyes <jhessaureyes@gmail.com>
2022-11-01 13:29:33 -0600

    origin

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const app = express();
 app.set("port", config.port);
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://services-app-frontend.vercel.app/",
     credentials: true,
   })
 );
