d2814f84d26fac433fedb34f76e88a61de47e528
JhessReyes <jhessaureyes@gmail.com>
2022-11-02 03:47:21 -0600

    get Objects

--- src/controllers/helper.controller.js
+++ src/controllers/helper.controller.js
@@ -27,3 +27,25 @@ export const getEntityById = async (req, res) => {
     }
   } else res.status(401).json({ message: "User Unauthorized", status: false });
 };
+
+export const getObjects = async (req, res) => {
+  const validated = await validateSession(req);
+  if (validated.status) {
+    try {
+      const { tableName } = req.params;
+      const pool = await getConection();
+      const result = await pool.request().query("SELECT * FROM " + tableName);
+      if (result.recordset[0] == undefined) {
+        res.status(404).json({ message: "Object Not Found", status: false });
+      } else {
+        res.status(200).json(result.recordset);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "Server Error",
+        error: error.message,
+      });
+    }
+  } else res.status(401).json({ message: "User Unauthorized", status: false });
+};
--- src/routes/helper.routes.js
+++ src/routes/helper.routes.js
@@ -1,8 +1,9 @@
 import Router from "express";
-import { getEntityById } from "../controllers/helper.controller";
+import { getEntityById, getObjects } from "../controllers/helper.controller";
 
 const router = Router();
 
 router.get("/getEntityById/:tableName/:id", getEntityById);
+router.get("/getObjects/:tableName", getObjects);
 
 export default router;
