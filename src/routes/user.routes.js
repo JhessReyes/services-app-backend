8e3fd21901047d1e2bed5712a8afa5fe00a4b141
JhessReyes <jhessaureyes@gmail.com>
2022-11-01 18:06:00 -0600

    a

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,15 +11,7 @@ import { statusChange } from "../controllers/status.controller";
 
 const router = Router();
 /* ROUTES FOR USERS */
-const cors = require("cors");
-app.use(
-  cors({
-    origin: "https://services-app-frontend.vercel.app",
-    credentials: true,
-    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-  })
-);
-router.get("/getUsers", cors(), getUsers);
+router.get("/getUsers", getUsers);
 router.get("/getUser/:id", getUserById);
 router.put("/updateUser", updateUser);
 router.post("/insertUser", insertUser);
